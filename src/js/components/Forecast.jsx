64c5e2ed3132348507cb218ddf99c45a07a2abf9
Eagle <eagle@eaglehunt.net>
2023-05-31 13:49:25 -0700

    Completed req

--- src/js/components/Forecast.jsx
+++ src/js/components/Forecast.jsx
@@ -3,22 +3,33 @@ import secrets from '../../../secrets.json';
 
 const Forecast = ({ zipCode }) => {
   const apiKey = secrets['apiKey'];
-  const [weatherData, setWeatherData] = useState(null);
+  const [forcastWeatherData, setForcastWeatherData] = useState(null);
   const [locationData, setLocationData] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [loadingLocationData, setLoadingLocationData] = useState(true);
+  const [loadingForcastWeatherData, setLoadingForcastWeatherData] = useState(true);
+
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const month = date.getMonth();
+    const day = date.getDate();
+    const dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][date.getDay()];
+
+    return `${dayOfWeek} ${day}/${month}`
+  }
+
 
   useEffect(() => {
     const getLocationData = () => {
       fetch(`http://dataservice.accuweather.com/locations/v1/postalcodes/search?apikey=${apiKey}&q=${zipCode}`)
         .then(response => response.json())
         .then(data => {
-          console.log(data)
           setLocationData(data);
-          setLoading(false); // Set loading to false once data is fetched
+          setLoadingLocationData(false); // Set loading to false once data is fetched
         })
         .catch(error => {
           console.error('Error:', error);
-          setLoading(false); // Set loading to false in case of an error
+          setLoadingLocationData(false); // Set loading to false in case of an error
         });
     };
 
@@ -27,40 +38,59 @@ const Forecast = ({ zipCode }) => {
 
   
   useEffect(() => {
-    const getWeatherData = (cityKey) => {
+    const getForcastWeatherData = (cityKey) => {
       fetch(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityKey}?apikey=${apiKey}`)
         .then(response => response.json())
         .then(data => {
-          console.log(data)
-          setWeatherData(data);
-          //setLoading(false); // Set loading to false once data is fetched
+          setForcastWeatherData(data);
+          setLoadingForcastWeatherData(false);
         })
         .catch(error => {
           console.error('Error:', error);
-          //setLoading(false); // Set loading to false in case of an error
+          setLoadingForcastWeatherData(false);
         });
     }
 
     if (locationData && locationData.length > 0){
       const cityKey = locationData[0]['ParentCity']['Key'];
-      getWeatherData(cityKey);
+      getForcastWeatherData(cityKey);
     }
-  })
+  }, [locationData, apiKey]);
 
-  console.log(weatherData)
-
+  console.log(forcastWeatherData)
   return (
     <div>
-      {loading ? (
-        <h1>...</h1> // put a component here to show loading
-      ) : locationData && locationData.length > 0 ? (
-        <>
+      {loadingLocationData ? (
+      <h1>...</h1> // put a component here to show loading
+    ) : locationData && locationData.length > 0 ? (
+      <>
         <h1>5 Day forecast for {locationData[0]['LocalizedName']}, {locationData[0]['AdministrativeArea']['ID']}</h1>
-
-        </>
-      ) : (
-        <h1>Invalid ZIP code</h1>
-      )}
+        {loadingForcastWeatherData ? (
+          <h2>...</h2>
+        ) : forcastWeatherData ? (
+          <>
+            <h2>{forcastWeatherData['Headline']['Text']}</h2>
+            {forcastWeatherData['DailyForecasts'].map((forecast, index) => (
+              <div key={index} className="weather-card" style={{
+                border: '1px solid black',
+                backgroundColor: 'lightblue',
+                padding: '10px',
+              }}>
+                <div>
+                  <div>{formatDate(forecast['Date'])}:</div>
+                  <div>Hi: {forecast['Temperature']['Maximum']['Value']}{'\u00B0'} {forecast['Temperature']['Maximum']['Unit']}</div>
+                  <div>Lo: {forecast['Temperature']['Minimum']['Value']}{'\u00B0'} {forecast['Temperature']['Minimum']['Unit']}</div>
+                </div>
+              </div>
+            ))}
+          </>
+        ) : (
+          <h2>No results found</h2>
+        )}
+      </>
+    ) : (
+      <h1>Invalid ZIP code</h1>
+    )}
     </div>
   );
 };
---
+++ src/js/components/ForecastCard.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const ForcastCard = () => {
+  
+  
+    return (
+      <>
+
+      </>
+    );
+  };
+
+export default ForcastCard;
